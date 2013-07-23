05000ee9c1f53121e0df759a0e1a5a48803ef8db
Scott Tanner <stanner92@gmail.com>
2013-07-23 11:45:22 -0400

    Removed Player message methods from log handler

--- src/net/ST392/ChangeHorse/CHLogHandler.java
+++ src/net/ST392/ChangeHorse/CHLogHandler.java
@@ -1,9 +1,6 @@
 package net.ST392.ChangeHorse;
 
 import java.util.logging.Logger;
-
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 
 public class CHLogHandler {
@@ -23,17 +20,6 @@ public class CHLogHandler {
 			return "[" + pdFile.getName() +"] (" + pdFile.getVersion() + ") " + message;
 		}
 		
-		//Generate message for player
-		private String buildStringPlayer(String message, ChatColor color) {
-			return color + message;
-		}
-
-		//Generate message for player
-		private String buildWarningPlayer(String message, ChatColor color) {
-			PluginDescriptionFile pdFile = plugin.getDescription();
-			return color + "[" + pdFile.getName() +"] " + message;
-		}
-		
 		//Log message as INFO
 		public void info(String message){
 			this.logger.info(this.buildString(message));
@@ -42,17 +28,6 @@ public class CHLogHandler {
 		//Log message as WARNING
 		public void warn(String message){
 			this.logger.warning(this.buildString(message));
-		}
-		
-		
-		//Normal message to the player
-		public void sendPlayerNormal(Player player, String message) {
-			player.sendMessage(this.buildStringPlayer(message, ChatColor.GREEN));
-		}
-		
-		//Normal message to the player
-		public void sendPlayerWarn(Player player, String message) {
-			player.sendMessage(this.buildWarningPlayer(message, ChatColor.YELLOW));
 		}
 
 }
